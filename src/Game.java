d7e7d8c60f21e488da92578b34643d26ab7ab6ff
Aaron <aarongabor@cmail.carleton.com>
2021-10-21 22:49:48 -0400

    V1.0 of game setup

--- src/Game.java
+++ src/Game.java
@@ -1,21 +1,51 @@
 import java.util.*;
 
-public class Game {
+public class Game
+{
     private List<Player> players;
     private Board board;
+    private Command command;
 
-    public Game(){
+    public Game()
+    {
         board = new Board();
         players = new ArrayList<>();
+        command = new Command();
     }
 
-    public void addPlayer(String name, String piece){
+    public void addPlayer(String name)
+    {
         Player p = new Player(name, board);
         players.add(p);
     }
 
-    public void play(){
-        //test
-        int i = 5;
+    public void play()
+    {
+
+    }
+
+    public static void main(String [] args)
+    {
+        Game game = new Game();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Number of Players: ");
+        int num = in.nextInt();
+        do
+        {
+            if (num < 2)
+            {
+                System.out.println("Error please enter a higher number");
+                num = in.nextInt();
+            }
+            else
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    game.addPlayer("Player " + i);
+                }
+            }
+        }while(num < 2);
+        in.close();
+        game.play();
     }
 }
