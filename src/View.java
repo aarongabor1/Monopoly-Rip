ff95e09d6f0773532997218dcb0f081000f27787
Aaron <aarongabor@cmail.carleton.com>
2021-11-08 18:51:03 -0500

    View V2.2.1- Comments Added and Player Selection Panel aesthetics fixed

--- src/View.java
+++ src/View.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 
 public class View
 {
-    private static Font font1 = new Font("Times New Roman", Font.BOLD, 75);
-    private static Font font2 = new Font("Times New Roman", Font.PLAIN, 25);
-    private static Font font3 = new Font("Times New Roman", Font.PLAIN, 15);
-    private static Font font4 = new Font("Times New Roman", Font.BOLD, 100);
-    private static Font font5 = new Font("Times New Roman", Font.BOLD, 50);
-
+    //Set fonts for all text in the GUI
+    private static final Font font1 = new Font("Times New Roman", Font.BOLD, 75);
+    private static final Font font2 = new Font("Times New Roman", Font.PLAIN, 25);
+    private static final Font font3 = new Font("Times New Roman", Font.PLAIN, 15);
+    private static final Font font4 = new Font("Times New Roman", Font.BOLD, 100);
+    private static final Font font5 = new Font("Times New Roman", Font.BOLD, 50);
+    private static final Font font6 = new Font("Times New Roman", Font.BOLD, 40);
 
     private JFrame frame;
     private JPanel mainPanel;
@@ -30,8 +31,13 @@ public class View
     private JButton submit;
     private JLabel selectionText;
 
+    /**
+     * A constructor for the View class that will create a GUI and will configure the settings
+     * of all the frames, panels, text areas, text fields, labels, and buttons.
+     */
     public View()
     {
+        //Creating the frame and the main panel
         frame = new JFrame("Monopoly");
         JPanel bottomPanel = new JPanel();
         frame.setSize(1000, 650);
@@ -39,84 +45,103 @@ public class View
         mainPanel = new JPanel(new GridLayout(3, 1));
         mainPanel.setVisible(true);
 
+        //Creates the player selection panel
         playerSelectionPanel = new JPanel();
         playerSelectionPanel.setLayout(new GridLayout(3, 1));
+        playerSelectionPanel.setBackground(Color.white);
         playerSelectionPanel.setVisible(true);
         frame.add(playerSelectionPanel);
 
+        //Creates the player selection label
         selectionText = new JLabel("Please Select The Amount of Players (2+):");
         selectionText.setHorizontalAlignment(JLabel.CENTER);
+        selectionText.setFont(font6);
 
+        //Creates the Player selection entering area
         playerSelection = new JTextField();
         playerSelection.setEditable(true);
         playerSelection.setActionCommand("Player Number");
-
-        submit = new JButton("Submit");
-
         playerSelection.setFont(font5);
 
+        //Creates a button to allow users to submit their number of players
+        submit = new JButton("Submit");
+        submit.setFont(font1);
+        submit.setBackground(Color.cyan);
+
+        //Adds all elements to the player selection panel
         playerSelectionPanel.add(selectionText);
         playerSelectionPanel.add(playerSelection);
         playerSelectionPanel.add(submit);
 
+        //Creates the output of game information
         output = new JTextArea();
         output.setFont(font2);
         output.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(output, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         mainPanel.add(scrollPane);
 
+        //Creates the panel that gives player information
         updatePanel = new JPanel(new GridLayout(1, 3));
 
+        //Creates the display for the player's money balance
         balance = new JTextField();
         balance.setFont(font1);
         balance.setEditable(false);
         balance.setBackground(Color.white);
         balance.setHorizontalAlignment(JTextField.CENTER);
 
+        //Creates the display for the player's name
         playerName = new JLabel();
         playerName.setVisible(true);
         playerName.setFont(font1);
         playerName.setBackground(Color.white);
 
+        //Creates the display for the properties that the player own
         properties = new JTextArea();
         properties.setFont(font3);
         properties.setEditable(false);
         JScrollPane scrollPane1 = new JScrollPane(properties, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
+        //Adds the three items above to the update panel
         updatePanel.add(balance);
         updatePanel.add(playerName);
         updatePanel.add(scrollPane1);
         mainPanel.add(updatePanel);
 
+        //Adds the bottom panel to the main panel
         mainPanel.add(bottomPanel);
         bottomPanel.setVisible(true);
 
+        //Creates the roll panel and the roll button
         rollPanel = new JPanel(new GridLayout(1, 2));
         rollButton = new JButton("Roll");
         rollButton.setEnabled(true);
         rollButton.setFont(font4);
         rollButton.setBackground(Color.CYAN);
-
         rollPanel.add(rollButton);
         rollPanel.setVisible(false);
         rollPanel.setBackground(Color.white);
         bottomPanel.add(rollPanel);
 
+        //Creates a button panel for the turn buttons
         buttonPanel = new JPanel(new GridLayout(1, 3));
         buttonPanel.setBackground(Color.white);
 
+        //Creates the buy button and adds it to the button panel
         buyButton = new JButton("Buy");
         buyButton.setEnabled(false);
         buyButton.setFont(font4);
         buyButton.setBackground(new Color(124, 252, 0));
         buttonPanel.add(buyButton);
 
+        //Creates the rent button and adds it to the button panel
         rentButton = new JButton("Pay Rent");
         rentButton.setEnabled(false);
         rentButton.setFont(font4);
         rentButton.setBackground(Color.ORANGE);
         buttonPanel.add(rentButton);
 
+        //Creates the end turn button and adds it to the button panel
         endTurnButton = new JButton("End Turn");
         endTurnButton.setEnabled(true);
         endTurnButton.setFont(font4);
@@ -127,24 +152,42 @@ public class View
         frame.setVisible(true);
     }
 
+    /**
+     * This method will update the output display by adding a String that it is passed
+     * and then will bring the panel to the end so that the play may see the most recent information.
+     * @param str The String that is to be added to the output display
+     */
     public void updateOutput(String str)
     {
         output.append(str + "\r\n");
         output.setCaretPosition(output.getDocument().getLength());
     }
 
+    /**
+     * This method will update the player's balance and will display the amount of money that the
+     * player currently has.
+     * @param str The String that contains the balance to be displayed that doesn't
+     *            have a dollar sign in it.
+     */
     public void updateBalance(String str)
     {
         balance.setText(null);
         balance.setText("$" + str + "\r\n");
     }
 
-    // Now updates the JLabel
+    /**
+     * This method will notify a player that has entered an invalid number in the player number
+     * window to reenter a valid number.
+     */
     public void inputFailed()
     {
         selectionText.setText("Please Enter a Valid Number of Players (2+):");
     }
 
+    /**
+     * This method will display all the properties that a player currently owns.
+     * @param arrayList The ArrayList of property objects of the property that are to be displayed.
+     */
     public void updateProperties(ArrayList<Property> arrayList)
     {
         properties.setText(null);
@@ -154,6 +197,10 @@ public class View
         }
     }
 
+    /**
+     * This is a method that will update the player name that is being displayed.
+     * @param str A String that contains the player name to be displayed.
+     */
     public void updatePlayerName(String str)
     {
         playerName.setText(str);
@@ -164,7 +211,11 @@ public class View
         return playerSelection.getText();
     }
 
-    // Put all ALs into one method for simplicity
+    /**
+     * A method that will add the action listener that has been passed to the elements of the
+     * GUI that needs an action listener.
+     * @param o The ActionListener object that contains the action listener to be used.
+     */
     public void monopolyActionListener(ActionListener o)
     {
         rollButton.addActionListener(o);
@@ -175,6 +226,10 @@ public class View
         rentButton.addActionListener(o);
     }
 
+    /**
+     * This is a method that will switch between the player selection part of the program and the
+     * main game part of the program and will allow the game to start.
+     */
     public void startGame()
     {
         frame.remove(playerSelectionPanel);
@@ -183,6 +238,10 @@ public class View
         frame.setSize(1000, 650);
     }
 
+    /**
+     * This method will change the bottom panel of the GUI to the roll panel and will disable the rent
+     * and buy buttons for the next player.
+     */
     public void setRoll()
     {
         buttonPanel.setVisible(false);
@@ -191,18 +250,26 @@ public class View
         rollPanel.setVisible(true);
     }
 
+    /**
+     * This method will switch the bottom panel from the roll panel to the button panel.
+     */
     public void setButtons()
     {
         rollPanel.setVisible(false);
         buttonPanel.setVisible(true);
-        buyButton.setEnabled(false);
     }
 
+    /**
+     * This method will enable the buy button
+     */
     public void setBuyable()
     {
         buyButton.setEnabled(true);
     }
 
+    /**
+     * This method will enable the Rent button
+     */
     public void setRentable()
     {
         rentButton.setEnabled(true);
@@ -229,7 +296,6 @@ public class View
         n.updateOutput("Test16");
         n.updateBalance("10000");
         n.updateBalance("20000");
-        n.startGame();
         n.setRoll();
         n.setButtons();
         n.setBuyable();
