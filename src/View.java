c7abe8e893eac1cd433733d6e97fe5d60c73fcae
CamSommer <CamSommerville@cmail.carleton.ca>
2021-11-22 21:24:25 -0500

    GUI no longer displays houses and hotels for squares that can't have them.

--- src/View.java
+++ src/View.java
@@ -291,7 +291,12 @@ public class View
         properties.setText(null);
         for (Property p : arrayList)
         {
-            properties.append(p.getName() + " Set: " + p.getSet() + " Houses: " + p.getHouses() + " Hotel: " + p.hasHotel() + "\n");
+            if (p instanceof Utility || p instanceof Railroad){
+                properties.append(p.getName() + "\n");
+            }
+            else {
+                properties.append(p.getName() + " Set: " + p.getSet() + " Houses: " + p.getHouses() + " Hotel: " + p.hasHotel() + "\n");
+            }
         }
     }
 
